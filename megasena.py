541ac4057a832315d743506bec5c8d6ba13ea04b
Joaquim <113483260+joaquim-de-melo-neto@users.noreply.github.com>
2023-11-20 19:00:10 -0300

    Update megasena.py

--- megasena.py
+++ megasena.py
@@ -1,6 +1,18 @@
 import random as ran
 
+def povoar_lista(qtd:int, lista:list):
+    contador = 0
+    while contador < qtd:
+        aleatorio = ran.randrange(1, 61)
+        if aleatorio not in lista:
+            lista.append(aleatorio)
+            contador += 1
+        else:
+            continue
+
+
 aposta = []
+numeros_sorteados = []
 
 while True:
     try:
@@ -11,14 +23,16 @@ while True:
     if qtd_numeros not in range(6,11):
         print("Os valores tem que ser de 6 a 10!")
         continue
-    contador = 0
-    while contador < qtd_numeros:
-        aleatorio = ran.randrange(1,61)
-        if aleatorio not in aposta:
-            aposta.append(aleatorio)
-            contador += 1
-        else:
-            continue
+
+    povoar_lista(qtd_numeros, aposta)
+    povoar_lista(qtd_numeros, numeros_sorteados)
+
     break
 
+acertos = 0
+for num in aposta:
+    if num in numeros_sorteados:
+        acertos += 1
 print(aposta)
+print(numeros_sorteados)
+print(f'Número de acertos: {acertos}')
