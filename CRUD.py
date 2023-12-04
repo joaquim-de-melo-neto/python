c22f7059058e6cf3c088653d547dcfa2fe9a4e1d
Joaquim <113483260+joaquim-de-melo-neto@users.noreply.github.com>
2023-12-04 18:57:21 -0300

    Update CRUD.py

--- CRUD.py
+++ CRUD.py
@@ -4,17 +4,43 @@ contatos = []
 def linha():
     print('\n--------------------------\n\n')
     
+def validar_digito(digito):
+    if digito.isdigit() and (0 < digito <= len(contatos)):
+        return True   
+    return False
 def adicionar_contato():
     print('--- ADICIONAR CONTATO ---')
     nome = input('Informe o nome: ')
     telefone = input('Informe o numero: ')
+    contatos.append(nome,telefone)
+    linha()
     
+def exibir_contatos():
+    contador = 1
+    for contato in contatos:
+        print('%.2d : %s' % contador, contato)
+        contador += 1
+        
+    linha() 
+    
+def deletar_contato():
+    exibir_contatos()
+    index = input('Informe o index do contato que deseja apagar: ')
+    validar_digito(index)
+    del contatos[int(index) - 1]
+
 while True:
     print('--- CRUD ---')
     print('1 - Adicionar contato\n2 - Exibir lista\n3 - Deletar contato\n4 - Sair\n')
     opcao = int(input('Informe a opção desejada: '))
-    
+
     match opcao:
         case 1:
             adicionar_contato()
-    linha()
+        case 2:
+            exibir_contatos()    
+        case 3:
+            deletar_contato()    
+        case 2:
+            exibir_contatos()    
+            
